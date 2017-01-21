67ad8667d07904e07eba42ed399bfa1e22923299
Widodo <rwidodo31@gmail.com>
2017-01-21 16:45:37 +0700

    Version 1

---
+++ app/src/main/java/com/bhinus/mybhinus/JadwalActivity.java
@@ -0,0 +1,13 @@
+package com.bhinus.mybhinus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JadwalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal);
+    }
+}
